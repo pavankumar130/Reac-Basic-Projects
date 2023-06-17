6bb27de023343731d98dfed63db7becc651b6b6c
pavankumar130 <20bcs130@iiitdmj.ac.in>
2023-06-17 09:59:42 +0530

    modified:   14. Cart/src/context.jsx

--- 14. Cart/src/context.jsx
+++ 14. Cart/src/context.jsx
@@ -1,6 +1,6 @@
-import { useContext, useReducer, useEffect, createContext } from 'react';
-import reducer from './reducer';
-import cartItems from './data';
+import { useContext, useReducer, useEffect, createContext } from 'react'
+import reducer from './reducer'
+import cartItems from './data'
 import {
   CLEAR_CART,
   REMOVE,
@@ -8,43 +8,41 @@ import {
   DECREASE,
   LOADING,
   DISPLAY_ITEMS,
-} from './actions';
-import { getTotals } from './utils';
-const url = 'https://www.course-api.com/react-useReducer-cart-project';
+} from './actions'
+import { getTotals } from './utils'
 
-const AppContext = createContext();
+const AppContext = createContext()
 
 const initialState = {
   loading: false,
-  cart: new Map(),
-};
+  cart: new Map(cartItems.map((item) => [item.id, item])),
+}
 
 export const AppProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(reducer, initialState);
-  const { totalAmount, totalCost } = getTotals(state.cart);
+  const [state, dispatch] = useReducer(reducer, initialState)
+  const { totalAmount, totalCost } = getTotals(state.cart)
 
   const clearCart = () => {
-    dispatch({ type: CLEAR_CART });
-  };
+    dispatch({ type: CLEAR_CART })
+  }
 
   const remove = (id) => {
-    dispatch({ type: REMOVE, payload: { id } });
-  };
+    dispatch({ type: REMOVE, payload: { id } })
+  }
   const increase = (id) => {
-    dispatch({ type: INCREASE, payload: { id } });
-  };
+    dispatch({ type: INCREASE, payload: { id } })
+  }
   const decrease = (id) => {
-    dispatch({ type: DECREASE, payload: { id } });
-  };
-  const fetchData = async () => {
-    dispatch({ type: LOADING });
-    const response = await fetch(url);
-    const cart = await response.json();
-    dispatch({ type: DISPLAY_ITEMS, payload: { cart } });
-  };
+    dispatch({ type: DECREASE, payload: { id } })
+  }
+  const fetchData = () => {
+    dispatch({ type: LOADING })
+    const cart = cartItems
+    dispatch({ type: DISPLAY_ITEMS, payload: { cart } })
+  }
   useEffect(() => {
-    fetchData();
-  }, []);
+    fetchData()
+  }, [])
   return (
     <AppContext.Provider
       value={{
@@ -59,9 +57,9 @@ export const AppProvider = ({ children }) => {
     >
       {children}
     </AppContext.Provider>
-  );
-};
+  )
+}
 
 export const useGlobalContext = () => {
-  return useContext(AppContext);
-};
+  return useContext(AppContext)
+}
